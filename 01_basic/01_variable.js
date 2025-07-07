86ffb2f9c1122cd4b312bc7c8ff7c025d3fd1429
Prince9028 <princebhavnagariya43@gmail.com>
2025-07-07 16:46:38 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,22 @@
+const accountid = 1244
+let accountemail = "princepatel@gmail.accountemail"
+var accountpass = "1234"
+accountcity = "lathidad"
+
+// accountid= 2
+
+accountemail="he@.com"
+
+accountpass="321"
+
+accountcity ="botad"
+
+console.log(accountid);
+console.log(accountemail);
+console.log(accountpass);
+console.log(accountcity);
+
+
+console.table([accountid,accountemail,accountpass,accountcity])
+
+
