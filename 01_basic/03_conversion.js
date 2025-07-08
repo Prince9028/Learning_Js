da33470e687ad9ecce0907727d0b72edbc5fc3b7
Prince9028 <princebhavnagariya43@gmail.com>
2025-07-08 12:58:32 +0000

    conversion

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,29 @@
+let score ="33aaa"
+
+console.log(typeof score);
+console.log(typeof (score));
+
+let valueofscore = Number (score)
+console.log(typeof valueofscore);
+console.log(valueofscore);
+
+// "33" => 33
+// "33abc" => NaN (not a number)
+// true => 1 , false => 0
+
+let ifdefine =2
+
+let booleanifdefine = Boolean(ifdefine)
+console.log(booleanifdefine);
+
+//1=> ture ; 0=>false
+//"" =>false
+//"prince "=> ture
+
+let number =23
+
+let stringnumber = String(number)
+console.log(stringnumber);
+console.log(typeof stringnumber);
+
+
