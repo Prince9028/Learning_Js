162d6379ad174553bb80cdd45693eb58da293d9a
Prince9028 <princebhavnagariya43@gmail.com>
2025-07-08 12:36:27 +0000

    datatype

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all js    code as newer version
+
+// alert( 3+3) // we are useing nodejs not browser
+
+console.log(3+3);   console.log("Prince");
+
+let name ="Prince"
+let age = 18
+let condistion = false
+
+// number => 2 to power 53
+//bigint
+//string==>""
+//boolean => ture/false
+//null => standalone value
+//undefined =>   ex: let stats;
+//symbol =>unique
+
+
+// object
+
+console.log(typeof undefined)
