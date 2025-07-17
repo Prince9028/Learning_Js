e5339cfa62ef805da1e9c4919e00c511d08447be
Prince9028 <princebhavnagariya43@gmail.com>
2025-07-17 17:22:45 +0000

    datatypesummary

---
+++ 01_basic/05_datatypesummary.js
@@ -0,0 +1,28 @@
+ // primitive
+ //  7 type :  string ,number,boolearn,null,undefined,symbol,bigint
+
+ const score =100
+ const scorevalue = 100.3
+
+ const isloggedin = false
+ const outsidetemp = null
+ let useremail;
+
+ const id = Symbol('123')
+ const anotherid = Symbol('123')
+ // reference (non primitiv)
+ //  ---> array,objects,function
+
+ const name  = ["rajdip","prince","yash"];
+   let myobj={
+    name : "prince",
+    age : 17
+ }
+
+
+ const myfunction = function(){
+   console.log("Hello Prince");
+   
+ }
+ console.log(typeof scorevalue);
+ 
