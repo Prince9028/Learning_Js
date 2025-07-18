f78d330aded2a5200fe7b8badb58fcdfbbe2c611
Prince9028 <princebhavnagariya43@gmail.com>
2025-07-18 13:56:22 +0000

    string

---
+++ 01_basic/06_string.js
@@ -0,0 +1,27 @@
+const name = "prince"
+const repcount = 50
+ 
+//console.log(name + repcount +"value");
+console.log(`Hi, my name is ${name} and  my repocount is ${repcount}`); 
+
+const world = new String('prince')
+
+console.log(world [0,3]);
+
+console.log(world.length);
+console.log(world.toUpperCase());
+
+console.log(world.substring(1,4)); 
+
+console.log(world.slice(1,3)); // reverce string 
+console.log(world ,world.trim()); // extra space delete
+
+const url ="https://www.youtube.com/prince%20patel" 
+// console.log(url.replace('youtube','prince'));
+
+console.log(world.split('_'));
+
+
+
+
+
