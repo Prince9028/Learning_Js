0868928f52893e0dbc2705f98e097ac8935dbc64
Prince9028 <princebhavnagariya43@gmail.com>
2025-07-19 09:45:02 +0000

    number and math

---
+++ 01_basic/07_num_and_maths.js
@@ -0,0 +1,32 @@
+const score = 400
+console.log(score);
+
+const balance = new Number(100)
+//console.log(balance)
+
+//console.log(balance.toFixed(3)); // point ki right point add
+
+const othernumber = 23.5536
+
+//console.log(othernumber.toPrecision(3)); // important
+
+const zero = 10000 
+//console.log(zero.toLocaleString('en-in'));
+
+//*************************************maths***********************
+
+console.log(Math);
+//console.log(Math.abs(-4)); // only nagetive value are positive
+//console.log(Math.round(6.4));
+//console.log(Math.ceil(4.2)); // up value accpted    
+//console.log(Math.floor(4.9)); //down valur are accpted
+//console.log(Math.pow(5,3));
+
+console.log((Math.random()*10 ) + 1) // random value are 0 to 1 value 
+
+const min =10
+const max = 20
+
+
+console.log(Math.random() * (max-min +1) + min)
+
